50447261d53f60c34688cfa22de14fe65a8f3611
pshrestha <pshrest2@go.olemiss.edu>
2022-07-22 15:44:41 -0500

    added db context for cosmos

---
+++ Repositories/Contexts/CosmosDbContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using ScannedAPI.Models;
+using ScannedAPI.Repositories.Contexts.Interfaces;
+
+namespace ScannedAPI.Repositories.Context
+{
+    public class CosmosDbContext : ICosmosDbContext
+    {
+        private readonly Container _container;
+
+        public CosmosDbContext(CosmosClient client, string databaseName, string containerName)
+        {
+            _container = client.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddItemAsync(ScannlyItem item)
+        {
+            await _container.CreateItemAsync(item, new PartitionKey(item.Id.ToString()));
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await _container.DeleteItemAsync<ScannlyItem>(id, new PartitionKey(id));
+        }
+
+        public async Task<ScannlyItem> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<ScannlyItem> response = await _container.ReadItemAsync<ScannlyItem>(id, new PartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<ScannlyItem>> GetItemsAsync(string queryString)
+        {
+            var query = _container.GetItemQueryIterator<ScannlyItem>(new QueryDefinition(queryString));
+            List<ScannlyItem> results = new List<ScannlyItem>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+        public async Task UpdateItemAsync(string id, ScannlyItem item)
+        {
+            await _container.UpsertItemAsync(item, new PartitionKey(id));
+        }
+    }
+}
+
---
+++ Repositories/Contexts/Interfaces/ICosmosDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ScannedAPI.Models;
+
+namespace ScannedAPI.Repositories.Contexts.Interfaces
+{
+    public interface ICosmosDbContext
+    {
+        Task<IEnumerable<ScannlyItem>> GetItemsAsync(string query);
+        Task<ScannlyItem> GetItemAsync(string id);
+        Task AddItemAsync(ScannlyItem item);
+        Task UpdateItemAsync(string id, ScannlyItem item);
+        Task DeleteItemAsync(string id);
+    }
+}
+
